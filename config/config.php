ba0a810d4bd69887d80ff3ee559615df93c2d4d5
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2015-09-17 15:03:11 +0200

    static dropzone css

--- config/config.php
+++ config/config.php
@@ -17,6 +17,6 @@ $GLOBALS['TL_FFL']['dropzone'] = 'HeimrichHannot\\DropZone\\FormDropZone';
 if(TL_MODE == 'FE')
 {
 	// Add the scripts
-	$GLOBALS['TL_CSS']['dropzone'] = 'assets/dropzone/' . $GLOBALS['TL_ASSETS']['DROPZONE'] . '/css/dropzone.min.css';
+	$GLOBALS['TL_CSS']['dropzone'] = 'assets/dropzone/' . $GLOBALS['TL_ASSETS']['DROPZONE'] . '/css/dropzone.min.css|screen|static|3.12.0';
 	$GLOBALS['TL_JAVASCRIPT']['dropzone'] = 'assets/dropzone/' . $GLOBALS['TL_ASSETS']['DROPZONE'] . '/js/dropzone' . (!$GLOBALS['TL_CONFIG']['debugMode'] ? '.min' : '') . '.js|static';
 }
